4381f6be63528029ed1fd1487ad590841bf0fd7a
Robson Harrison <robson.harrison@sky.uk>
2023-02-09 17:07:43 +0000

    End of day commit and push

--- src/main/java/com/sky/springBootTest/controller/QuoteController.java
+++ src/main/java/com/sky/springBootTest/controller/QuoteController.java
@@ -2,17 +2,37 @@ package com.sky.springBootTest.controller;
 import com.sky.springBootTest.model.Quote;
 import com.sky.springBootTest.repository.QuoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.http.ResponseEntity;
+
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 public class QuoteController {
+
+    @GetMapping("/data")
+    @ResponseBody
+    public String getData() {
+        // Retrieve data from the database and return it in JSON format
+        return "{\"data\": [{\"name\": \"Robson\", \"age\": 30}, {\"name\": \"Jane\", \"age\": 25}]}";
+    }
+
+
+
+
     @Autowired
     private QuoteRepository quoteRepository;
+
+
+
 
     @GetMapping("/quotes")
     public List<Quote> getQuotes(@RequestParam("search") Optional<String> searchParam){
@@ -32,8 +52,13 @@ public class QuoteController {
         return quoteRepository.save(q);
     }
 
+
     @RequestMapping(value="/quotes/{quoteId}", method=RequestMethod.DELETE)
     public void deleteQuote(@PathVariable(value = "quoteId") Long id) {
         quoteRepository.deleteById(id);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteController.class, args);
     }
 }
--- src/main/java/com/sky/springBootTest/repository/QuoteRepository.java
+++ src/main/java/com/sky/springBootTest/repository/QuoteRepository.java
@@ -5,9 +5,11 @@ import com.sky.springBootTest.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
     @Query("SELECT q FROM Quote q WHERE q.quote LIKE %?1%")
     List<Quote> getContainingQuote(String word);
